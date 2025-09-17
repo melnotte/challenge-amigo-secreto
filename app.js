83b5b9888fa610f671de37c6c0d0614f80f878fe
melnotte <200300633@ucaribe.edu.mx>
2025-09-16 20:28:43 -0500

    Sube repositorio inicial con archivos base del proyecto.

---
+++ app.js
@@ -0,0 +1,14 @@
+
+//Función para agregar nombres
+//Los usuarios escribirán el nombre de un amigo en un campo de texto y lo agregarán a una lista visible al hacer clic en "Adicionar".
+
+//Validar entrada: Si el campo de texto está vacío, el programa mostrará una alerta pidiendo un nombre válido.
+
+
+
+
+//Función para mostrar nombres
+//Visualizar la lista: Los nombres ingresados aparecerán en una lista debajo del campo de entrada.
+
+//Función para sortear nombres
+//Sorteo aleatorio: Al hacer clic en el botón "Sortear Amigo", se seleccionará aleatoriamente un nombre de la lista y se mostrará en la página.
